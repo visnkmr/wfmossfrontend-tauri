787145ac12fdac20acafdabe58d9481fa2672028
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-07-07 01:38:04 +0530

    based on latest release @ https://codeberg.org/visnk/wfmossfrontend

---
+++ src/_next/static/MoJCfQbMTfCW7EEWknsea/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e95edc5f443cbd3f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ src/_next/static/chunks/pages/_app-b9a87c52205ebbe4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{4864(_,p,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",()=>w(8648)])}},_=>{var p=p=>_(_.s=p),w=(p(4864),p(2259));_N_E=w}]);
